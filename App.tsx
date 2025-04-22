0f6b03b73107c5b6a3829802f239b9c73127b9ab
tomohiro1024 <gongyezhihong732@gmail.com>
2025-04-22 18:17:13 +0900

    フォーマッター

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{height: 100,width: "10%",backgroundColor: "orange"}}></View>
+      <View style={{ height: 100, width: "10%", backgroundColor: "orange" }}></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  itemContainer: { height: 100, width: "10%", backgroundColor: "orange" },
 });
