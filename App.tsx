1baab481fd190ebd43a911262fcf382d22527726
tomohiro1024 <gongyezhihong732@gmail.com>
2025-04-22 15:45:35 +0900

    仮

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{height: 100,width: "10%",backgroundColor: "orange"}}></View>
       <StatusBar style="auto" />
     </View>
   );
