35a9351d6b4d9c600bd66e7cc90d878b0fe7e380
tomohiro1024 <gongyezhihong732@gmail.com>
2025-04-22 18:20:21 +0900

    テストコミット

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{ height: 100, width: "10%", backgroundColor: "orange" }}></View>
+      <View style={{ height: 100, width: "100%", backgroundColor: "orange" }}></View>
       <StatusBar style="auto" />
     </View>
   );
