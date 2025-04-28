ccc666d05a3876970210be12552211715ccbd1dd
tomohiro1024 <gongyezhihong732@gmail.com>
2025-04-28 17:18:45 +0900

    UI

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Image, View } from "react-native";
+import { StyleSheet, Image, View, Text } from "react-native";
 
 export default function App() {
+  const testText =
+    "カメラレンズを購入したはずが。被害額は2万円超えです。どういった経緯で詐欺に遭ったのか、どのような対応をしたかとともに、フリマアプリで同様の被害に合わない方法をお伝えします。";
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
@@ -11,7 +13,12 @@ export default function App() {
             source={{ uri: "https://picsum.photos/seed/picsum/300/300" }}
           />
         </View>
-        <View style={styles.rightContainer}></View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text} numberOfLines={4}>
+            {testText}
+          </Text>
+          <Text style={styles.subText}>集英社</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -28,9 +35,17 @@ const styles = StyleSheet.create({
   itemContainer: {
     height: 100,
     width: "100%",
-    backgroundColor: "orange",
+    backgroundColor: "#f0f0f0",
     flexDirection: "row",
   },
-  leftContainer: { height: 90, width: 90, backgroundColor: "green", margin: 5, borderRadius: 10 },
-  rightContainer: { flex: 1, height: 100, width: "100%", backgroundColor: "blue" },
+  leftContainer: { height: 90, width: 90, margin: 5, borderRadius: 10 },
+  rightContainer: { flex: 1, marginTop: 5, justifyContent: "space-between" },
+  text: {
+    fontSize: 14,
+  },
+  subText: {
+    fontSize: 12,
+    color: "grey",
+    marginBottom: 5,
+  },
 });
